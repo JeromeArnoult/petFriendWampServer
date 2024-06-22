84a07d713ba2fa466a4cfa3b4fdd07d5b109a88b
JeromeArnoult <jerome.arnoult.pro@proton.me>
2024-06-22 10:42:32 +0200

    Mise en place detail d'un article

--- src/Controller/Blog/PostController.php
+++ src/Controller/Blog/PostController.php
@@ -1,11 +1,12 @@
 <?php
 namespace App\Controller\Blog;
 
+use App\Entity\Post;
 use App\Repository\PostRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 
 class PostController extends AbstractController
 {
@@ -17,6 +18,13 @@ class PostController extends AbstractController
 
         return $this->render('pages/post/index.html.twig',[
             'posts' => $postRepository->findPublished($request->query->getInt('page', 1))
+        ]);
+    }
+    #[Route('/article/{slug}', name:'post.show',methods:['GET'] )]
+    public function show(Post $post) : Response
+    {   
+        return $this->render('pages/post/show.html.twig', [
+            'post' => $post
         ]);
     }
 }
